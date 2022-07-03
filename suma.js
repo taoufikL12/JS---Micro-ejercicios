28dcea40e7737e60a48b07a84c3f4c2e9666a5d0
sniper <mensitaoufik29@gmail.com>
2022-07-03 21:01:42 +0200

    lo importante es intentarlo

---
+++ suma.js
@@ -0,0 +1,14 @@
+var n1 = parseInt(prompt("Introcuce un numero"));
+var n2 = prompt("intruduce otro numero");
+var r = n1 + parseInt(n2);
+
+alert (r);
+
+var suma = function (numero1,numero2){
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero1 = parseFloat(document.getElementById("numero2").value);
+    var resultado = numero1 + numero2;
+    return resultado;
+
+}
+
